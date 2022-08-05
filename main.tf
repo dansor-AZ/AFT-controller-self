3b6d336116c0e4db210a1277425577e307dfda40
Gandhi <vgn@3c0630478548.ant.amazon.com>
2022-08-05 13:48:24 +1000

    .terraform.tf removed

--- main.tf
+++ main.tf
@@ -27,24 +27,24 @@ module "vpc" {
 module "aft_pipeline" {
   source = "aws-ia/control_tower_account_factory/aws"
   # Required Variables
-  ct_management_account_id                         = 145306892140
-  log_archive_account_id                           = 091833507420
-  audit_account_id                                 = 393160057906
-  aft_management_account_id                        = 758391798493
-  ct_home_region                                   = ap-southeast-2
-  tf_backend_secondary_region                      = ap-southeast-1
+  ct_management_account_id = 145306892140
+  log_archive_account_id = 091833507420
+  audit_account_id = 393160057906
+  aft_management_account_id = 758391798493
+  ct_home_region = ap-southeast-2
+  tf_backend_secondary_region = ap-southeast-1
   
   # Terraform variables
-  terraform_version                                = "0.15.5"
-  terraform_distribution                           = "oss"
+  terraform_version = "0.15.5"
+  terraform_distribution = "oss"
     
   # VCS variables
-  vcs_provider                                     = "github"
+  vcs_provider = "github"
 
   # AFT Feature flags
-  aft_feature_cloudtrail_data_events               = false
-  aft_feature_enterprise_support                   = false
-  aft_feature_delete_default_vpcs_enabled          = false
+  aft_feature_cloudtrail_data_events = false
+  aft_feature_enterprise_support = false
+  aft_feature_delete_default_vpcs_enabled = false
 }
 
 
