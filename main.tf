c0c610256907b7098ae673ec8c9ee3fc839090bf
dansor <dansor@amazon.com>
2022-10-07 16:22:23 +1300

    test

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "aft_pipeline" {
   ct_management_account_id = "412634494108"
   log_archive_account_id = "744322718174"
   audit_account_id = "128980538509"
-  aft_management_account_id = "937699266191"
+  aft_management_account_id = "412634494108"
   ct_home_region = "us-east-1"
   tf_backend_secondary_region = "us-east-2"
   
